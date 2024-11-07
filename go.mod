c450d621c067f88b6918c95be1f96fcc7c80a067
Etrinque <Solvdinc@outlook.com>
2024-11-07 18:21:28 -0500

    url normalization logic-req added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/etrinque/go-crawl
 
 go 1.22.0
+
+require golang.org/x/net v0.30.0 // indirect
--- normalize_url.go
+++ normalize_url.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func NormalizeURL(url string) (string, error) {
@@ -19,4 +23,56 @@ func NormalizeURL(url string) (string, error) {
 		return normalized, nil
 	}
 	return normURL, nil
+}
+
+
+func getUrlsFromHTML(htmlBody string) ([]string, error) {
+	var linkNodes []string
+
+	htmlReader := strings.NewReader(htmlBody)
+	node, err := html.Parse(htmlReader)
+	if err != nil {
+		return []string{}, fmt.Errorf("failure to parse body. error: %v", err)
+		
+	}
+
+	var recurse func(*html.Node) 
+	recurse = func(n *html.Node){
+		
+		if n.Type == html.ElementNode && n.Data == "a" {
+			linkNodes = append(linkNodes, n.Data)
+		}
+		if n.NextSibling == nil {
+			return
+		}
+		recurse(n.NextSibling)
+	}
+	recurse(node)
+
+	for _, elem := range linkNodes {
+		fmt.Println(elem)
+	}
+
+	return linkNodes, nil
+}
+
+func clientReq(url string) error {
+	var reader strings.Reader
+	var client *http.Client
+
+	req, err := http.NewRequest("GET", url, &reader)
+	if err != nil {
+		return fmt.Errorf("error making request: %v", err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("Request failure: %v", err)
+	}
+
+	
+
+	getUrlsFromHTML()
+
+	return nil
 }
