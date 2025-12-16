983f8770d938f7452f6ab20f1eba8ca4b458c4b2
Eric Trinque <TrinqueEric@gmail.com>
2025-12-16 06:27:54 -0500

    refactored for clarity

--- gethtml.go
+++ gethtml.go
@@ -5,41 +5,36 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"time"
 )
 
-func GetHtml(rawUrl string) (string, error) {
-	var rawHTML string
-	var client http.Client
-
-	//Use http.Get to fetch the webpage of the rawURL
-	//Return an error if the HTTP status code is an error-level code 400+
+func getHtml(rawUrl string) (string, error) {
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
 
 	resp, err := client.Get(rawUrl)
-	if err != nil || resp.StatusCode > 399 {
-		errLog = append(errLog, fmt.Errorf("get raw url failed: %w", err))
-		return "", err
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			errLog = append(errLog, fmt.Errorf("error closing response body: %w", err))
-		}
-	}(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("HTTP request failure: %w", err)
 
-	//Return an error if the response content-type header is not text/html
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("HTTP request failed with status code: %d", resp.StatusCode)
+	}
+
+	// verify content type
 	contentType := resp.Header.Get("Content-Type")
-	if !strings.Contains(contentType, "text/html") {
-		errLog = append(errLog, fmt.Errorf("expected -> text/html | got -> %s", contentType))
-		return "", err
+	if !strings.Contains(strings.ToLower(contentType), "text/html") {
+		return "", fmt.Errorf("expected -> text/html | got -> %s", contentType)
 	}
 
 	//Return the webpage's HTML if successful
 	b, err := io.ReadAll(resp.Body)
 	if err != nil {
-		errLog = append(errLog, fmt.Errorf("error reading response body: %w", err))
-		return "", err
+		return "", fmt.Errorf("error reading response body: %w", err)
 	}
-	rawHTML = string(b)
 
-	return rawHTML, nil
+	return string(b), nil
 }
